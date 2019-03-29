31b4901fd753bf6ac159679e7fa7412f7562ffb9
Steff Kelsey <sk@lavjaveler.com>
2019-03-29 00:57:40 -0400

    Nicely solved for JS
    Parameterized tests with jest-each
    Sweet, sweet action

--- src/fizzbuzz/__tests__/index.spec.js
+++ src/fizzbuzz/__tests__/index.spec.js
@@ -1,9 +1,54 @@
-import fizzNumber from "../";
+import fizzBuzz from "../"
+import each from 'jest-each'
 
-describe("fizzNumber", () => {
+describe("fizzBuzz", () => {
 
-  it("should work.", () => {
-    expect(fizzNumber(1)).toBeUndefined();
-  });
+	each([
+		[1, "1"],
+		[2, "2"],
+		[4, "4"],
+		[97, "97"]
+	]).it("when given a non-matching number returns stringified number", (input, expected) => {
 
+			const actual = fizzBuzz(input)
+
+			expect(actual).toBe(expected)
+	})
+
+	each([
+		[3],
+		[6],
+		[9]
+	]).it("when given a multiple of 3 returns 'fizz'", (input) => {
+		const expected = "fizz"
+
+		const actual = fizzBuzz(input)
+
+		expect(actual).toBe(expected)
+	})
+
+	each([
+		[5],
+		[10],
+		[20]
+	]).it("when given a multiple of 5 returns 'buzz'", (input) => {
+		const expected = "buzz"
+
+		const actual = fizzBuzz(input)
+
+		expect(actual).toBe(expected)
+	})
+
+	each([
+		[15],
+		[30],
+		[45]
+	]).it("when given a multiple of 3 and 5 returns 'fizzbuzz'", (input) => {
+		const expected = "fizzbuzz"
+
+		const actual = fizzBuzz(input)
+
+		expect(actual).toBe(expected)
+	})
 });
+
--- src/fizzbuzz/index.js
+++ src/fizzbuzz/index.js
@@ -1,2 +1,14 @@
 export default function fizzBuzz(number) {
+	let result = "";
+	if (number % 3 == 0) {
+		result += "fizz";
+	}
+	if (number % 5 == 0) {
+		result += "buzz";
+	}
+	if (result == "") {
+		result = number.toString();
+	}
+	return result;
 }
+
